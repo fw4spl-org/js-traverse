8e2c1d7131902b8b6782af1e9c17268257da6e30
Anders D. Johnson <adjohnson916@users.noreply.github.com>
2015-07-25 20:07:42 -0500

    Add tests for async update and context.

--- test/async-each.js
+++ test/async-each.js
@@ -5,12 +5,47 @@ var util = require('util');
 
 test('async each', function (t) {
     t.plan(1);
-    
+
     var obj = { x : 3 };
     traverse(obj).forEach(function (x, cb) {
-      cb(null);
+        cb();
     }, function (err, newObj) {
         t.pass();
-        console.log('done', arguments);
+    });
+});
+
+test('async each context', function (t) {
+    t.plan(1);
+
+    var obj = { x: { y: 3 }};
+    traverse(obj).forEach(function (x, cb) {
+        console.log(this.node);
+        console.log(this.path);
+        console.log(this.key);
+        console.log(this.isRoot);
+        console.log(this.notRoot);
+        console.log(this.isLeaf);
+        console.log(this.notLeaf);
+        console.log(this.level);
+        console.log(this.circular);
+        cb();
+    }, function (err, newObj) {
+        t.pass();
+    });
+});
+
+test('async each update', function (t) {
+    t.plan(1);
+
+    var obj = { x: { y: 3 }};
+    traverse(obj).forEach(function (x, cb) {
+        if (this.key === 'y') {
+            this.update({z: 0});
+        }
+        cb();
+    }, function (err, newObj) {
+        t.pass();
+        console.log(JSON.stringify(obj, null, '  '));
+        console.log(JSON.stringify(newObj, null, '  '));
     });
 });
--- test/async-map.js
+++ test/async-map.js
@@ -5,13 +5,28 @@ var util = require('util');
 
 test('async map', function (t) {
     t.plan(1);
-    
+
     var obj = { x : 3 };
     traverse(obj).map(function (x, cb) {
-      this.update('a');
-      cb(null);
+        this.update('a');
+        cb();
     }, function (err, newObj) {
         t.pass();
-        console.log('done', arguments);
+    });
+});
+
+test('async map update', function (t) {
+    t.plan(1);
+
+    var obj = { x: { y: 3 }};
+    traverse(obj).map(function (x, cb) {
+        if (this.key === 'y') {
+            this.update({z: 0});
+        }
+        cb();
+    }, function (err, newObj) {
+        t.pass();
+        console.log(JSON.stringify(obj, null, '  '));
+        console.log(JSON.stringify(newObj, null, '  '));
     });
 });
